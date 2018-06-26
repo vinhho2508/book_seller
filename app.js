76c08a70495d1a78dc7883fe42b960d5adc334de
phuvinh2211 <35805931+phuvinh2211@users.noreply.github.com>
2018-06-26 12:13:31 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,49 +1,58 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var catalogRouter=require('./routes/catalog');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/node-auth')
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
-var mongoose = require('mongoose');
-//var dev_db_url = 'mongodb://localhost:@:27017/local_library'
-//var mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect('mongodb://localhost/book_sell');
-//mongoose.createConnection('mongodb://localhost:27017/local_library').host;
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog',catalogRouter);
+app.use('/', index);
+app.use('/users', users);
 
-app.get('/signup', function(req, res){
-  res.render('signup');
-});
-app.get('/login', function(req, res){
-  res.render('login');
+// passport configuration
+var User = require('./models/User');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.get('/admin', function(req, res){
-  res.render('admin');
-});
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
